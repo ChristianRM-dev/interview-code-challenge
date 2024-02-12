9f5d74648d12ef7845ef0929e1b53b47ae9bace8
ArielRetamarTR <ariel.retamar@transactcampus.com>
2024-02-12 12:18:15 -0300

    fixes routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: './challenge-1',
+    redirectTo: 'challenge-1',
     pathMatch: 'full'
   },
   {
